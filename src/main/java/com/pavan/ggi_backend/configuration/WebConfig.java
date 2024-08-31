b0060477cbb1acfaef4d998a5cdf33a5d7eba26d
Pavan R <pavankumarvk867@gmail.com>
2024-08-31 13:21:44 +0530

    remove unused import

--- src/main/java/com/pavan/ggi_backend/configuration/WebConfig.java
+++ src/main/java/com/pavan/ggi_backend/configuration/WebConfig.java
@@ -1,5 +1,4 @@
 package com.pavan.ggi_backend.configuration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // your React app's URL
+                .allowedOrigins("http://localhost:3000") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
